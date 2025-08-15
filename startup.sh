c663aa2a337576dcedb66915a35bf85c7a90ea4b
TaljaaEb <138305368+TaljaaEb@users.noreply.github.com>
2025-08-15 07:55:54 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,1 +1,8 @@
-python3.10 -m http.server 8000 -d $home -b 0.0.0.0 && python3.10 b_collector_monitor.py
+# A 
+/usr/bin/python3.9 a_sql_monitor.py
+
+# B
+python3.10 -m http.server 8000 -d $HOME -b 0.0.0.0 && python3.10 b_collector_monitor.py
+
+# C
+#C:\Users\<username>\AppData\Local\Programs\Python\Python<XY>\python.exe b_collector_monitor.py
