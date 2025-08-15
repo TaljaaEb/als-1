4858762d52244f9db5e903af7269b208fef88b32
TaljaaEb <138305368+TaljaaEb@users.noreply.github.com>
2025-08-15 07:59:15 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,6 +3,9 @@
 
 # B
 python3.10 -m http.server 8000 -d $HOME -b 0.0.0.0 && python3.10 b_collector_monitor.py
+#C:\Users\<username>\AppData\Local\Programs\Python\Python<XY>\python.exe b_collector_monitor.py
 
 # C
-#C:\Users\<username>\AppData\Local\Programs\Python\Python<XY>\python.exe b_collector_monitor.py
+C:\Users\<username>\AppData\Local\Programs\Python\Python<XY>\python.exe c_compare_logger.py
+
+
